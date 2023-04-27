09cb500ea7b0224973f4aad5dc6d06afc89dcb28
janghanna <ewal4291@gmail.com>
2023-04-27 18:41:35 +0900

    feat : detail 페이지 초기 셋팅

--- src/page/Detail/Detail.tsx
+++ src/page/Detail/Detail.tsx
@@ -1,6 +1,17 @@
 const Detail = () => {
 
+    return (
+        <div>
+            <div>날짜</div>
 
+            <div>이름:</div>
+            <div>성별:</div>
+            <div>나이:</div>
+            <div>주민번호:</div>
+            <div>전화번호:</div>
+            <div>메모:</div>
+        </div>
+    )
 }
 
 export default Detail;
