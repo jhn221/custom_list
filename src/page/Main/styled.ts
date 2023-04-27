f9661d6d41c34e79cbd0409c6a0e9774b56d8ece
janghanna <ewal4291@gmail.com>
2023-04-27 21:58:35 +0900

    style : main 페이지 css 수정

--- src/page/Main/styled.ts
+++ src/page/Main/styled.ts
@@ -8,7 +8,7 @@ export const Main = styled.div`
 `
 export const advertisementMemo = styled.div`
     border: 1px solid black;
-    width: 30%;
+    width: 35%;
     height: 100%;
 `
 
@@ -22,22 +22,57 @@ export const Memo = styled.div`
 
 export const customer = styled.div`
     border: 1px solid black;
-    width: 30%;
+    width: 45%;
+`
+
+export const dateFilter = styled.div`
+    display: flex;
+    margin: 0 15px;
+    display: flex;
+    /* align-items: baseline */
+    h5{
+        width: 50%;
+    }
+    /* .input-datepicker{
+        margin: 20px;
+    } */
+    .react-datepicker-wrapper {
+        display: inline-block;
+        padding: 0;
+        border: 0;
+        width: 100%;
+        height: 23px;
+    }
+    .react-datepicker-popper[data-placement^=top] .react-datepicker__triangle::before, .react-datepicker-popper[data-placement^=bottom] .react-datepicker__triangle::before, .react-datepicker-popper[data-placement^=top] .react-datepicker__triangle::after, .react-datepicker-popper[data-placement^=bottom] .react-datepicker__triangle::after {
+        border: 0px solid transparent;
+    }
 `
 
 export const CustomerList = styled.div`
-    width: 100%;
-    height: 20%;
+    width: 92%;
+    height: 10%;
     background-color: aliceblue;
     display: flex;
     flex-direction: column;
+    border: 1px solid black;
+    margin: 20px;
+    font-size: 20px;
     span{
         display: flex;
         margin: 20px;
+    }
+    div{
+        margin-left: 20px;
     }
 `
 
 export const Search = styled.div`
     border: 1px solid black;
-    width: 30%;
+    width: 20%;
+    h5{
+        margin-left: 15px;
+    }
+    div{
+        margin: 20px;
+    }
 `
