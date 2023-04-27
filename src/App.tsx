3ff74ba64434147336a0360bd81f8bf4506cd731
janghanna <ewal4291@gmail.com>
2023-04-27 15:08:29 +0900

    refactor: 불필요한 코드 삭제 및 component 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header/Header';
+import Main from './page/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          Learn React
+        <Header/>
+        <Main/>
       </header>
     </div>
   );
