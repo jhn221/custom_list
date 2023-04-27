315539b29c33aaac8e9fcd775c846c74878affa9
janghanna <ewal4291@gmail.com>
2023-04-27 18:41:59 +0900

    style : main 화면 css 수정

--- src/page/Main/styled.ts
+++ src/page/Main/styled.ts
@@ -8,21 +8,36 @@ export const Main = styled.div`
 `
 export const advertisementMemo = styled.div`
     border: 1px solid black;
+    width: 30%;
     height: 100%;
 `
 
 export const advertisement = styled.div`
-
+    width: 100%;
 `
 
 export const Memo = styled.div`
-
+    margin: 30px 0 0 10px ;
 `
 
 export const customer = styled.div`
-    border: 1px solid black;  
+    border: 1px solid black;
+    width: 30%;
+`
+
+export const CustomerList = styled.div`
+    width: 100%;
+    height: 20%;
+    background-color: aliceblue;
+    display: flex;
+    flex-direction: column;
+    span{
+        display: flex;
+        margin: 20px;
+    }
 `
 
 export const Search = styled.div`
     border: 1px solid black;
+    width: 30%;
 `
