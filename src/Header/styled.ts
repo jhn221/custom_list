bb169c9e315bfc6b6da1c899818c0806a4741c1d
janghanna <ewal4291@gmail.com>
2023-04-27 21:51:17 +0900

    style : header 정렬

--- src/Header/styled.ts
+++ src/Header/styled.ts
@@ -4,4 +4,17 @@ export const HeaderContainer = styled.div`
     width: 100%;
     height: 2em;
     background-color: #d2d2d2;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .searchBar{
+        width: 70%;
+        height: 50%;
+    }
+    button {
+        width: 5%;
+        height: 50%;
+        border-radius: 20px;
+        margin-right: 20px;
+    }
 `
