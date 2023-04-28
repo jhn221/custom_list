c5887444a883648fee56a3a9d69c49193c93d577
janghanna <ewal4291@gmail.com>
2023-04-28 23:17:44 +0900

    style : slick arrow css 변경

--- src/component/AdList/styled.ts
+++ src/component/AdList/styled.ts
@@ -3,35 +3,38 @@ import styled from "styled-components";
 
 export const Listcontainer = styled.div`
     width: 100%;
-    margin-left:10%;
+    margin-left:60px;
     .item{
         width: 80%;
         height: 100%;
     }
     .slick-prev:before,
-    .slick-next:before
-{
-    font-family: 'slick';
-    font-size: 20px;
-    line-height: 1;
+    .slick-next:before{
+        font-family: 'slick';
+        font-size: 20px;
+        line-height: 1;
+        opacity: 0.9;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    .slick-prev{
+        left: 15px;
+        z-index: 2;
+    }
 
-    opacity: .5;
-    color: red;
-
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
+    .slick-next{
+        left: 500px;
+    }
 `
 
 export const eachAd = styled.div`
-    width: 100px;
-    height: 500px;
     img{
         height: 400px;
         display: flex;
         justify-content: center;
     }
     div{
+        margin-top: 20px;
         text-align: center;
     }
 `
