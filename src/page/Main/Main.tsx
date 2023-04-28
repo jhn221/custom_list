c8ec44f03287ee3d1493f5773871d88ee31497a7
janghanna <ewal4291@gmail.com>
2023-04-28 17:08:50 +0900

    fix: memo 컴포넌트 분리 및 코드 정리

--- src/page/Main/Main.tsx
+++ src/page/Main/Main.tsx
@@ -1,18 +1,12 @@
 import * as S from './styled';
-
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AdList from '../../component/AdList/AdList';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Link } from 'react-router-dom';
-import Detail from '../Detail/Detail';
-import { fileURLToPath } from 'url';
-
-interface memolist {
-    id:number,
-    value:string,
-}
+import Memo from '../../component/Memo/memo';
+import {FaCalendarAlt} from 'react-icons/fa';
 
 interface list {
     userid:string,
@@ -25,22 +19,20 @@ interface list {
 
 const Main = () => {
 
-    // console.log(props)
-    const [memo, setMemo] = useState<memolist[]>([{id:0, value:''}])
-    const [selectedDate, setselectedDateDate] = useState<string>('');
+    const [selectedDate, setselectedDate] = useState<string>('');
     const [customer, setCustomer] = useState<list[]>([]);
     const [change, selectChange] = useState<string>('name');
     const [infoValue, setInfoValue] = useState<string>("")
     const [data, setData] = useState("");
 
-    console.log(customer)
+    // console.log(customer)
 
     const changeDate = (date:any) => {
         var year = date.getFullYear();
         var month = ('0' + (date.getMonth() + 1)).slice(-2);
         var day = ('0' + date.getDate()).slice(-2);
         var yyyyMMdd = year + month  + day;
-        setselectedDateDate(yyyyMMdd)
+        setselectedDate(yyyyMMdd)
     }
     localStorage.setItem("Date",selectedDate)
 
@@ -48,7 +40,6 @@ const Main = () => {
         axios
         .get(`http://34.22.82.239:8080/getUserList?date=${selectedDate}`)
         .then((res) => {
-            console.log(res.data.users)
             setCustomer(res.data.users)
             setData(res.data.users) 
         })
@@ -57,40 +48,17 @@ const Main = () => {
         });
     },[selectedDate])
 
-    // props.setData(data);
-
-      const memoChange = (e:any) => {
-        // setMemo(...memo,id:id+1,value: e.target.value)
-        console.log(memo)
-      }
-        const handleLogin = (e:any) => {
-            setMemo({
-                ...memo,
-                // value: e.target.value,
-            })
-        }
-
-        // console.log(memo)
-        const enterKeyPress = (e:any) => {
-        if (e.key === "Enter") {
-          handleLogin(e);
-        }}
-
         const changeInfoValue = (e:any) => {
             setInfoValue(e.target.value)
         }
 
-        let filterList = customer.filter((customer) => customer[change]===infoValue)
-        // console.log(filterList)
-        // console.log(change)
+        let filterList = customer.filter((customer) => customer[change] === infoValue)
 
         const seleteOption = (e:any) => {
             selectChange(e.target.value)
-            // console.log(e.target.value)
         }
         const nowDate = new Date();
- 
-        
+
 
     return(
         <S.Main>
@@ -100,31 +68,12 @@ const Main = () => {
                         <AdList/>
                 </S.advertisement>
                 <div className='title'>메모란</div>
-                <S.Memo>
-                    <S.MemoList>
-                        <div>dkdkk</div>
-                        <button>x</button>
-                    </S.MemoList>
-                    <S.MemoList>
-                        <div>dkdkk</div>
-                        <button>x</button>
-                    </S.MemoList>
-                </S.Memo>
-                    <S.MemoWrite>
-                        {memo.map((memo) => (
-                            <div>{memo.value}</div>
-                        ))}
-                        <input
-                        placeholder='메모 입력란'
-                        onChange={memoChange}
-                        onKeyPress={enterKeyPress}
-                        ></input>
-                        <button onClick={() => handleLogin}>작성</button>
-                    </S.MemoWrite>
+                <Memo/>
             </S.advertisementMemo>
             <S.customer>
                 <S.dateFilter>
-                    <h5>손님 {customer.length === undefined ? 0 : customer.length}명</h5>
+                    <h4>손님 {customer.length === undefined ? 0 : customer.length}명</h4>
+                    <label> 
                     <DatePicker
                         dateFormat="yyyyMMdd" // 날짜 형식 설정
                         className="input-datepicker" // 클래스 명 지정 css주기 위해
@@ -132,15 +81,17 @@ const Main = () => {
                         // selected={selectedDate}// value  // 날짜를 선택하였을 때 실행될 함수
                         onChange={(date: Date) => changeDate(date)}
                         />
+                    <FaCalendarAlt/>
+                    </label>
                 </S.dateFilter>
               {customer.length !== undefined ? customer.map((customer) => (
-                  <S.CustomerList>
+                  <S.CustomerList key={customer.userid}>
                       <Link to={`/detail/${customer.userid}`}>
                       <span>
                         <div>{customer.name}</div> | 
                         <div>
-                            {Number(customer.patDob.slice(7)) %2 === 0 ? "여" : "남"}
-                            /{
+                            {Number(customer.patDob.slice(7)) %2 === 0 ? "여" : "남"}/
+                            {
                                 Number(customer.patDob.substring(0,2)) > 23 ? 
                                 nowDate.getFullYear() - (Number(customer.patDob.substring(0,2)) + 1900) :
                                 nowDate.getFullYear() - (Number(customer.patDob.substring(0,2)) + 2000) }
@@ -154,7 +105,7 @@ const Main = () => {
               )):null}
             </S.customer>
             <S.Search>
-                <h5>손님 검색 필터</h5>
+                <h4>손님 검색 필터</h4>
                 <div>
                 <select onChange={seleteOption}>
                     <option value="name">이름</option>
@@ -167,7 +118,7 @@ const Main = () => {
                 </input>
                 </div>
                 {filterList.length !== undefined ? filterList.map((filterList) => (
-                  <S.SearchCustomerList>
+                  <S.SearchCustomerList key={filterList.userid}>
                       <Link to={`/detail/${filterList.userid}`}>
                       <span>
                         <div>{filterList.name}</div> |
