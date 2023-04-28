0c41ad3955a06ba97236bd6b891d4a4c8bed7d5a
janghanna <ewal4291@gmail.com>
2023-04-28 23:17:24 +0900

    fix : 메모 수정 시 메모 전체가 선택되는 issue 핸들링

--- src/component/Memo/memo.tsx
+++ src/component/Memo/memo.tsx
@@ -11,6 +11,7 @@ const Memo = () => {
     const [memos, setMemos] = useState<MemoType[]>([]);
     const [inputMemo, setInputMemo] = useState<string>("");
     const [edit, setEdit] = useState<boolean>(false);
+    const [selectedMemoId, setSelectedMemoId] = useState<number | null>(null);
 
     useEffect(() => {
         const memoData = localStorage.getItem("memos");
@@ -25,20 +26,17 @@ const Memo = () => {
     }, [memos]);
 
     const handleAddMemo = () => {
+      if(inputMemo === "")
+      {alert("검색어를 입력하세요")}
+      else{
         const newMemo: MemoType = {
         id: new Date().getTime(),
         content: inputMemo,
         };
         setMemos([...memos, newMemo]);
         setInputMemo("");
+      }
     };
-
-    const handleEnter = (e:any) => {
-        console.log(e)
-        if(e.key ==="Enter"){
-            handleAddMemo()
-        }
-    }
 
     const handleEditMemo = (id: number, newContent: string) => {
       setEdit(true)
@@ -60,51 +58,53 @@ const Memo = () => {
         const filteredMemos = memos.filter((memo) => memo.id !== id);
         setMemos(filteredMemos);
     };
-    console.log(memos)
 
-    const handleEditEnter = (e:any) => {
-      console.log(e)
-      if(e.key ==="Enter"){
-          setEdit(false)
-      }
-  }
-
-    return(
-        <S.MemoArea>
-            <S.Memo>
-              {memos.map((memo) => (
-                <div>
-                  {edit ? 
-                    <S.MemoList key={memo.id}>
-                      <input
-                        type="text"
-                        value={memo.content}
-                        onKeyPress={handleEditEnter}
-                        onChange={(event) => handleEditMemo(memo.id, event.target.value)}
-                      />
-                      <button onClick={() => setEdit(false)}>완료</button>
-                    </S.MemoList>
-                    : 
-                    <S.MemoList>
-                      <div onClick={() => setEdit(!edit)}>{memo.content}</div>
-                      <button onClick={() => handleDeleteMemo(memo.id)}>x</button>
-                    </S.MemoList>
-                  }
-                </div>
-              ))}
-            </S.Memo>
-            <S.MemoWrite>
+  return (
+    <S.MemoArea>
+      <S.Memo>
+        {memos.map((memo) => (
+          <S.MemoList key={memo.id}>
+            {selectedMemoId !== memo.id && (
+              <S.EachMemo>
+                <div onClick={() => setSelectedMemoId(memo.id)}>{memo.content}</div>
+                <button onClick={() => handleDeleteMemo(memo.id)}>x</button>
+              </S.EachMemo>
+            )}
+            {selectedMemoId === memo.id && (
+              <S.Edit>
                 <input
-                placeholder='메모 입력란'
-                type="text" 
-                value={inputMemo}
-                onChange={(e) => setInputMemo(e.target.value)}
-                onKeyPress={handleEnter} />
-                <button onClick={handleAddMemo}>작성</button>
-            </S.MemoWrite>
-        </S.MemoArea>
-    )
-
+                  type="text"
+                  value={memo.content}
+                  onChange={(e) => handleEditMemo(memo.id, e.target.value)}
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      handleEditMemo(memo.id, memo.content);
+                      setSelectedMemoId(null);
+                    }
+                  }}
+                />
+                <button onClick={() => setSelectedMemoId(null)}>완료</button>
+              </S.Edit>
+            )}
+          </S.MemoList>
+        ))}
+      </S.Memo>
+      <S.MemoWrite>
+        <input
+          placeholder="메모 입력란"
+          type="text"
+          value={inputMemo}
+          onChange={(e) => setInputMemo(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleAddMemo();
+            }
+          }}
+        />
+        <button onClick={handleAddMemo}>작성</button>
+      </S.MemoWrite>
+    </S.MemoArea>
+)
 }
 
 export default Memo;
--- src/component/Memo/styled.ts
+++ src/component/Memo/styled.ts
@@ -7,23 +7,46 @@ export const MemoArea = styled.div`
 
 export const Memo = styled.div`
     width: 95%;
-    height: 90%;
+    height: 14em;
     margin: 10px 0 0 10px ;
     border: 1px solid black;
     overflow: scroll;
-    div{
-        margin-bottom: 10px;
-    }
 `
 export const MemoList = styled.div`
     width: 90%;
-    height: 10%;
+    height: 40px;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     margin: 25px;
     border: 1px solid black;
+    div{
+        margin: 0 10px;
+    }
     input{
-        width: 100%;
+        width: 85%;
+        height: 30px;
+        border: 1px solid #d2d2d2;
+    }
+    button{
+        background-color: #ffff;
+        border: 0;
+        font-size: 15px;
+        margin-right:15px;
+    }
+`
+
+export const EachMemo = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`
+
+export const Edit = styled.div`
+    width: 100%;
+    display: flex;
+    button{
+        margin-left: 20px;
     }
 `
 
