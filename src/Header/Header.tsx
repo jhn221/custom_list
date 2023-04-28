4cc79d0fbfec5f5fad6b92b85679849dca212ba6
janghanna <ewal4291@gmail.com>
2023-04-28 23:15:40 +0900

    feat : 뒤로가기/앞으로가기 버튼 구현

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,22 +1,30 @@
 import * as S from './styled';
 import { ImArrowLeft, ImArrowRight, ImCross } from 'react-icons/im';
-import { AiOutlineHome } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { AiTwotoneHome } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    //뒤로가기 버튼
+    const backBtn = () =>  {
+        navigate(-1)
+    }
+    //앞으로가기 버튼
+    const frontBtn = () =>  {
+        navigate(-1)
+    }
+
     return (
         <S.HeaderContainer>
-            <ImArrowLeft/>
-            <ImArrowRight/>
+            <ImArrowLeft onClick={backBtn} style={{"color":"white","marginLeft":"10px"}}/>
+            <ImArrowRight onClick={frontBtn} style={{"color":"white"}}/>
             <ImCross/>
             <Link to='/'>
-                <AiOutlineHome/>
+                <AiTwotoneHome style={{"color":"white"}}/>
             </Link>
-            <input className='searchBar'>
-            </input>
+            <input className='searchBar'/>
             <button>🔍</button>
-
-
         </S.HeaderContainer>
     )
 
