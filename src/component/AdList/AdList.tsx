d6a1422424cd2d376b544b4eec178608f7c954ea
janghanna <ewal4291@gmail.com>
2023-04-29 10:18:31 +0900

    fix : map key값 추가 및 코드 설명 주석 추가

--- src/component/AdList/AdList.tsx
+++ src/component/AdList/AdList.tsx
@@ -6,56 +6,56 @@ import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 interface list {
-    adid:Number,
+    adid: Number,
     imageLink: string,
-    title: string
+    title: string,
 }
 const AdList  = () => {
 
-    const [adList, setAdList] = useState<list[]>([])
+  const [adList, setAdList] = useState<list[]>([])
 
+  //광고 api 호출
+  useEffect(()=> {
+    axios
+    .get(`http://34.22.82.239:8080/getAdList`)
+    .then((res) => {
+      setAdList(res.data.ads)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  },[])
 
-    useEffect(()=> {
-        axios
-        .get(`http://34.22.82.239:8080/getAdList`)
-        .then((res) => {
-          setAdList(res.data.ads)
+  //slider setting
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 50,
+    autoplay: true,
+    pauseOnHover: true,
+  };
 
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      },[])
-
-      const settings = {
-        dots: false,
-        infinite: true,
-        speed: 50,
-        autoplay: true,
-        pauseOnHover: true,
-      };
-
-      return(
-          <S.Listcontainer>
-            <Slider
-                className="item"
-                slidesToShow={1}
-                slidesToScroll={1}
-                {...settings}
-            >
-                {adList && adList.map((list) => (
-                    <S.eachAd>
-                        <img
-                        src={list.imageLink}
-                        alt="상품 이미지"
-                        className='itempicture'
-                        ></img>
-                        <div>{list.title}</div>
-                    </S.eachAd>
-                ))}
-            </Slider>
-          </S.Listcontainer>
-      )
+  return(
+    <S.Listcontainer>
+      <Slider
+        className="item"
+        slidesToShow={1}
+        slidesToScroll={1}
+        {...settings}
+      >
+        {adList && adList.map((list) => (
+          <S.eachAd key={`${list.adid}`}>
+            <img
+              src={list.imageLink}
+              alt="상품 이미지"
+              className='itempicture'
+            ></img>
+            <div>{list.title}</div>
+          </S.eachAd>
+        ))}
+      </Slider>
+    </S.Listcontainer>
+  )
 }
 
 export default AdList;
